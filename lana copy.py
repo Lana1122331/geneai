1ecc6ccae62288ba7421aa1babf2251808504071
Lana1122331 <iamyounala@gmail.com>
2025-07-11 01:34:46 +0300

    Update lana copy.py

--- lana copy.py
+++ lana copy.py
@@ -124,13 +124,13 @@ if dna_normal and dna_mutated:
     valid = all(base in "ATCGatcg" for base in dna_normal + dna_mutated)
     if not valid:
         st.error("❌ تأكد أن الحروف هي فقط A, T, C, G.")
-            else:
-                mrna = transcribe_dna_to_mrna(mutated_dna)
-                protein_normal = translate_mrna_to_protein(transcribe_dna_to_mrna(normal_dna))
-                protein_mutated = translate_mrna_to_protein(mrna)
-                mutation_type = detect_mutation_type(normal_dna, mutated_dna)
-                diagnosis = get_known_diagnosis(normal_dna[3:6], mutated_dna[3:6])
-                risk = estimate_risk(protein_normal, protein_mutated)
+        else:
+             mrna = transcribe_dna_to_mrna(mutated_dna)
+             protein_normal = translate_mrna_to_protein(transcribe_dna_to_mrna(normal_dna))
+            protein_mutated = translate_mrna_to_protein(mrna)
+            mutation_type = detect_mutation_type(normal_dna, mutated_dna)
+             diagnosis = get_known_diagnosis(normal_dna[3:6], mutated_dna[3:6])
+            risk = estimate_risk(protein_normal, protein_mutated)
 
     # 💡 عرض النتائج
     st.markdown('<div class="result-box">', unsafe_allow_html=True)

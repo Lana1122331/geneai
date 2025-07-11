4c3e938d931d8ad70b8768b9122fad7483b4f85f
Lana1122331 <iamyounala@gmail.com>
2025-07-11 13:59:09 +0300

    Update lana copy.py

--- lana copy.py
+++ lana copy.py
@@ -40,13 +40,30 @@ body, .stApp {
 
 # جدول الكودونات
 codon_table = {
-    'AUG': 'Methionine', 'UUU': 'Phenylalanine', 'UUC': 'Phenylalanine',
-    'UUA': 'Leucine', 'UUG': 'Leucine', 'CUU': 'Leucine', 'CUC': 'Leucine',
+    'UUU': 'Phenylalanine', 'UUC': 'Phenylalanine',
+    'UUA': 'Leucine', 'UUG': 'Leucine',
+    'CUU': 'Leucine', 'CUC': 'Leucine', 'CUA': 'Leucine', 'CUG': 'Leucine',
+    'AUU': 'Isoleucine', 'AUC': 'Isoleucine', 'AUA': 'Isoleucine',
+    'AUG': 'Methionine',
+    'GUU': 'Valine', 'GUC': 'Valine', 'GUA': 'Valine', 'GUG': 'Valine',
     'UCU': 'Serine', 'UCC': 'Serine', 'UCA': 'Serine', 'UCG': 'Serine',
-    'UAU': 'Tyrosine', 'UAC': 'Tyrosine', 'UGU': 'Cysteine', 'UGC': 'Cysteine',
-    'UGG': 'Tryptophan', 'UAA': 'STOP', 'UAG': 'STOP', 'UGA': 'STOP',
+    'AGU': 'Serine', 'AGC': 'Serine',
+    'CCU': 'Proline', 'CCC': 'Proline', 'CCA': 'Proline', 'CCG': 'Proline',
+    'ACU': 'Threonine', 'ACC': 'Threonine', 'ACA': 'Threonine', 'ACG': 'Threonine',
+    'GCU': 'Alanine', 'GCC': 'Alanine', 'GCA': 'Alanine', 'GCG': 'Alanine',
+    'UAU': 'Tyrosine', 'UAC': 'Tyrosine',
+    'UAA': 'STOP', 'UAG': 'STOP', 'UGA': 'STOP',
+    'CAU': 'Histidine', 'CAC': 'Histidine',
+    'CAA': 'Glutamine', 'CAG': 'Glutamine',
+    'AAU': 'Asparagine', 'AAC': 'Asparagine',
+    'AAA': 'Lysine', 'AAG': 'Lysine',
+    'GAU': 'Aspartic Acid', 'GAC': 'Aspartic Acid',
     'GAA': 'Glutamic Acid', 'GAG': 'Glutamic Acid',
-    'GUU': 'Valine', 'GUC': 'Valine', 'GUA': 'Valine', 'GUG': 'Valine'
+    'UGU': 'Cysteine', 'UGC': 'Cysteine',
+    'UGG': 'Tryptophan',
+    'CGU': 'Arginine', 'CGC': 'Arginine', 'CGA': 'Arginine', 'CGG': 'Arginine',
+    'AGA': 'Arginine', 'AGG': 'Arginine',
+    'GGU': 'Glycine', 'GGC': 'Glycine', 'GGA': 'Glycine', 'GGG': 'Glycine'
 }
 
 known_mutations = {
